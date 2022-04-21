77d95dddac1016b4055c4a6845181215ee77a828
Mohammed AlSheikh <alsheikhdev@gmail.com>
2022-04-21 07:47:45 +0300

    Fix typo in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # Seed roles
 
-Role.find_or_create_by(name: 'Teacher')
-Role.find_or_create_by(name: 'Student')
+Role.find_or_create_by(name: 'Teachers')
+Role.find_or_create_by(name: 'Students')
 
 # Seed for the first user (teacher) in the system e.g teacher@example.com
 
-teacher_role = Role.where(name: "Teacher").first_or_create
+teacher_role = Role.where(name: "Teachers").first_or_create
 User.create_with(password: "12345678", role_id: teacher_role.id).first_or_create(email: "teacher@example.com")
     
